4d92ede4f36b57308a66988e5615425f3ac77e40
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-11-21 01:07:13 +0700

    auto redirection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,14 @@
-
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function Custom404() {
+    // melakukan auto redirect ke halaman home setelah 2 detik
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 2000)
+    }, [])
     return (
         <div>
             <h1 className="title-not-found">Oops..!</h1>
