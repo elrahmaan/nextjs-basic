d90f0aa6bff4e77b39c00a946d4e9b2fb9613cf4
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-11-21 01:35:17 +0700

    optimalization image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,54 @@
 import Layout from "../component/Layout";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
 
     <Layout pageTitle="Home Page">
+      <Image src="/profile.jpg" width={200} height={200} alt="profile" />
+
+      {/* tanpa menggunakan image component dari nextJs (resource lebih besar)*/}
+      <img src="/profile.jpg" style={{ width: 200, height: 200 }} alt="profile" />
       <h1 className={styles['title-homepage']}>Welcome elrahmaan</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+
+      <Image src="/post-1.jpg" width={200} height={200} alt="post image 1" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+
+      <Image src="/post-2.jpg" width={200} height={200} alt="post image 2" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ipsa iusto natus. Vel, quos. Eaque nulla esse similique, saepe iure eveniet, magnam quidem itaque atque porro ut distinctio iste.</p>
     </Layout>
   );
 }
